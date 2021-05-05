3cd09ef74f84f7af6092ec4a4c2d7e81f9631388
marahmusleh <>
2021-05-05 13:26:02 +0000

    some changes

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+var userName = prompt("Please ENTER YOUR NAME.","")
+if( userName >= 'a' || userName >='A'){
+alert("WELCOME,"+userNameName+"!");
+}
+else
+{ alert("WELCOME!");}
+confirm("PLEASE RATE OUR WEBSITE.")
+}
+
