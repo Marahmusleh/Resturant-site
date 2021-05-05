ca502156e5d8d447861db4b73d7c54db8aa09dea
marahmusleh <>
2021-05-05 14:00:55 +0000

    make changes

---
+++ app.js
@@ -0,0 +1,18 @@
+
+  
+var userName = prompt("Please ENTER YOUR NAME.","");
+if( userName >= 'a' || userName >='A')
+{
+alert("WELCOME,"+userName+"!");
+}
+else
+{ alert("WELCOME!");}
+confirm("PLEASE RATE OUR WEBSITE.");
+var userNAme = prompt("Do your prefer our burger?")
+if (userName == 'yes')
+{
+  alert("GET 20% off all Entrees!");
+}
+else
+{ alert("SEE OUR NEW MENU!");}
+
