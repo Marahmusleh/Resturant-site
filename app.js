54e5f1a15324048123d981ba299fec121fd35f8c
marahmusleh <>
2021-05-06 12:46:07 +0000

    changes

--- app.js
+++ app.js
@@ -7,8 +7,26 @@ alert("WELCOME,"+userName+"!");
 else
 { alert("WELCOME!");}
 confirm("PLEASE RATE OUR WEBSITE.");
-var userNAme = prompt("Whats your favourite Burger?")
 alert('GET 20% off all Entrees')
 
 
 
+
+var favburger = prompt("Do your prefer Chicken alfredo burger or Classic Beef Burger?")
+
+while(favburger !== 'chicken alfredo burger' && favburger !== 'classic beef burger'){
+  favburger = prompt("Please choose between chicken alfredo burger and classic beef burger")
+}
+
+var userInput = prompt("How many meals do you want?")
+for (var i =0;i<=userInput;i++)
+{
+  if(favburger =='chicken alfredo burger'){
+  document.write(i+"-")
+  document.write('<img src = "https://twokooksinthekitchen.com/wp-content/uploads/2018/03/IMG_7838-3.jpg">')
+}
+else{
+  document.write(i+"-")
+  document.write('<img src = "https://www.crushpixel.com/big-static7/preview4/big-burger-with-french-fries-170857.jpg">')
+}
+}
